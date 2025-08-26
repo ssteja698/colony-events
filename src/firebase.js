44539156b40689702e1de1fd49cdfe004f50b033
Sai Siva Teja <ssteja2205@gmail.com>
2025-08-26 22:45:22 +0530

    caution

---
+++ src/firebase.js
@@ -0,0 +1,28 @@
+import {
+  APP_ID,
+  DOMAIN_ID,
+  FIREBASE_API_KEY,
+  MEASUREMENT_ID,
+  PROJECT_ID,
+  SENDER_ID,
+  STORAGE_BUCKET,
+} from "@env";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: DOMAIN_ID,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
